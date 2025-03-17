440451a679adad053ab51cde266083333c5a7e31
Pawan K Verma <parssyiodgn@gmail.com>
2025-03-17 12:51:01 +0530

    new files are added

---
+++ js/script.js
@@ -0,0 +1,91 @@
+let userscore = 0;
+let compscore = 0;
+
+const options = document.querySelectorAll(".options");
+const uscore = document.querySelector("#userScore");
+const cscore = document.querySelector("#compScore");
+const msg = document.querySelector("#msg");
+
+
+//Generating Computer Random Select
+const compNum = () => {
+    const compOps = ["rock", "paper", "scissor"];
+    let randomNum = Math.floor(Math.random() * 3);
+    return compOps[randomNum];
+} 
+
+//Checking what Computer Select
+let compSelect;                             
+const playGame = () =>{
+    compSelect = compNum();
+}
+
+//Disable button
+const disabledOps = () =>{
+    for (box of boxes){
+        box.disabled = true;
+    }
+
+    turn.classList.add("hide");
+}
+
+
+//Check on each condition who will win
+options.forEach((option) =>{
+    option.addEventListener("click", () =>{
+        const userSelect = option.getAttribute("id");
+        playGame();
+
+        if(userSelect === compSelect){
+            drawGame();
+        } else{
+            let userWin = true;
+
+            if(userSelect === "rock"){
+                userWin = compSelect === "paper" ? false : true;
+            } else if(userSelect === "paper"){
+                userWin = compSelect === "scissor" ? false : true;
+            } else{
+                userWin = compSelect === "rock" ? false : true;
+            }
+
+            winner(userWin);
+        } 
+        
+    });
+});
+
+
+const disabledbox = () =>{
+    for (option of options){
+        option.disabled = true;
+    }
+}
+
+let drawGame = () =>{
+    msg.innerHTML = `It's Draw! You both selected: ${compSelect}`
+}
+
+let winner = (userWin) =>{
+    if(userWin){
+        uscore.innerHTML = userscore += 1;
+        msg.innerHTML = `You Win! Computer Selected: ${compSelect}`
+        seriesWin();
+    } else{
+        cscore.innerHTML = compscore += 1;
+        msg.innerHTML = `You lose! Computer Selected: ${compSelect}`
+        seriesWin();
+    }
+}
+
+let seriesWin = () =>{
+    if(userscore === 10){
+        msg.innerHTML = `You Win! The Series by ${userscore} : ${compscore}`
+        disabledbox();
+    } else if(compscore === 10){
+        msg.innerHTML = `You Lost! The Series by ${compscore} : ${userscore}`
+        disabledbox();
+    }
+}
+
+
