128cdd48f275cc167a652034244c453cd958f86d
Pawan K Verma <parssyiodgn@gmail.com>
2025-03-18 12:40:12 +0530

    add New game button

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const options = document.querySelectorAll(".options");
 const uscore = document.querySelector("#userScore");
 const cscore = document.querySelector("#compScore");
 const msg = document.querySelector("#msg");
+const msgTxt = document.querySelector(".msgtxt");
+const btn = document.querySelector("button");
 
 
 //Generating Computer Random Select
@@ -55,6 +57,9 @@ options.forEach((option) =>{
     });
 });
 
+btn.addEventListener("click", () => {
+    location.reload(); // Reloads the current page
+});
 
 const disabledbox = () =>{
     options.forEach (option =>{
@@ -63,7 +68,7 @@ const disabledbox = () =>{
 }
 
 let drawGame = () =>{
-    msg.innerHTML = `It's Draw!<br> <span>You both selected: ${compSelect} </span>`
+    msgTxt.innerHTML = `It's Draw!<br> <span>You both selected: ${compSelect} </span>`
 }
 
 
@@ -71,22 +76,22 @@ let drawGame = () =>{
 let winner = (userWin) =>{
     if(userWin){
         uscore.innerHTML = userscore += 1;
-        msg.innerHTML = `You Win!<br> <span>Computer Selected: ${compSelect} </span>`
+        msgTxt.innerHTML = `You Win!<br> <span>Computer Selected: ${compSelect} </span>`
         seriesWin();
     } else{
         cscore.innerHTML = compscore += 1;
-        msg.innerHTML = `You Lose!<br> <span>Computer Selected: ${compSelect} </span>`
+        msgTxt.innerHTML = `You Lose!<br> <span>Computer Selected: ${compSelect} </span>`
         seriesWin();
     }
 }
 
 let seriesWin = () =>{
     if(userscore === 10){
-        msg.innerHTML = `<span>CONGRATULATION <br><br></span>You Win! <br> <span>The Series by ${userscore} : ${compscore}</span>`
+        msgTxt.innerHTML = `<span>CONGRATULATION <br><br></span>You Win! <br> <span>The Series by ${userscore} : ${compscore}</span>`
         msg.classList.add("up");
         disabledbox();
     } else if(compscore === 10){
-        msg.innerHTML = `<span>SORRY <br><br></span>You Lost! <br> <span>The Series by ${compscore} : ${userscore}</span>`
+        msgTxt.innerHTML = `<span>SORRY <br><br></span>You Lost! <br> <span>The Series by ${compscore} : ${userscore}</span>`
         msg.classList.add("down");
         disabledbox();
     }
